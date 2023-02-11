6f249a101acaadcf5ef2beebfa7fae30c9f83a62
Rnxp <90277569+SrXitme@users.noreply.github.com>
2023-02-11 01:41:45 +0000

    v1

---
+++ src/scripts/main.js
@@ -0,0 +1,19 @@
+function mod(){
+    let cnt = document.querySelector('#cnt').value
+    let cnt2 = document.querySelector('#cnt2').value
+    let cnt3 = document.querySelector('#cnt3').value
+    let cnt4 = document.querySelector('#cnt4').value
+    let out = document.querySelectorAll('.conf-border')
+    let border = document.querySelector('#div-radius')
+    let btn = document.querySelector('#btn-radius')
+    border.style.borderRadius = `${cnt}px ${cnt2}px  ${cnt3}px  ${cnt4}px`
+    btn.style.borderRadius = `${cnt}px ${cnt2}px  ${cnt3}px  ${cnt4}px`
+   
+    out[0].innerHTML = cnt
+    out[1].innerHTML = cnt2
+    out[2].innerHTML = cnt3
+    out[3].innerHTML = cnt4
+    
+    
+    
+}
